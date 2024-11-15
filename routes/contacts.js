404c38f4c15a9e1f9a755c1c4401e16ad8e7bbd6
ryleigh rowell <ryleighjean15@gmail.com>
2024-11-14 18:00:35 -0700

    put, post, delete routes added

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,5 +7,11 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
+router.post('/', contactsController.createUser);
+
+router.put('/:id', contactsController.updateUser);
+
+router.delete('/:id', contactsController.deleteUser);
+
 module.exports = router;
 
